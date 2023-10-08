232d3a39aca4c5af457780a42ed7bd789fbc1bef
Ramon Rodrigues <ramonrwx@gmail.com>
2023-10-07 23:58:52 -0300

    create utils

---
+++ worker/src/utils.js
@@ -0,0 +1,26 @@
+export async function handleFetchData(response) {
+  const { headers } = response;
+  const contentType = headers.get("content-type") || "";
+
+  if (contentType.includes("application/json")) {
+    return response.json();
+  } else {
+    return response.text();
+  }
+}
+
+export function responseWith(body, status) {
+  return new Response(body, {
+    status,
+    headers: {
+      "content-type": "text/plain;charset=UTF-8",
+    },
+  });
+}
+
+export const status = {
+  BadRequest: 400,
+  InternalError: 500,
+  NotFound: 404,
+  OK: 200,
+};
