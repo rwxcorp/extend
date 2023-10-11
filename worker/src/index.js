88ddfd76aa4d38f51c1412ee08168f48409c28b2
Ramon Rodrigues <ramonrwx@gmail.com>
2023-10-11 10:06:42 -0300

    create pastebin route and format using standard
    - improve routers

--- worker/src/index.js
+++ worker/src/index.js
@@ -1,18 +1,38 @@
-import { Router } from "@tsndr/cloudflare-worker-router";
-import { twitchAuthHandle, createClipHandle } from "./twitch.js";
+import { Router } from '@tsndr/cloudflare-worker-router'
+import { responseWith, status } from './utils.js'
+import { twitchAuthHandle, createClipHandle } from './twitch.js'
+import pastebinHandle from './pastebin.js'
 
-const router = new Router();
-router.cors(); // enable cors
+const router = new Router()
+router.cors() // enable cors
 
-router.get("/", function () {
-  return new Response("Hello World", { status: 200 });
-});
+router.get('/auth/callback', twitchAuthHandle)
+router.get('/clip/create/:id', createClipHandle)
+router.get('/clip/command/:id', function ({ req }) {
+  const url = new URL(req.url)
+  const id = req.params.id
 
-router.get("/auth/callback", twitchAuthHandle);
-router.get("/clip/create/:id", createClipHandle);
+  return responseWith(
+    `$(customapi.https://${url.host}/clip/create/${id}?channel=$(channel))`,
+    status.OK
+  )
+})
+router.get('/login', function ({ env, req }) {
+  const url = new URL(req.url)
+  const twitchURL = 'https://id.twitch.tv/oauth2/authorize?response_type='
+  const type = 'code'
+  const clientId = `&client_id=${env.TWITCH_CLIENT_ID}`
+  const redirectURI = `&redirect_uri=https://${url.host}/auth/callback`
+  const scope = '&scope=clips:edit'
+
+  const authLink = `${twitchURL}${type}${clientId}${redirectURI}${scope}`
+
+  return Response.redirect(authLink, status.Redirect)
+})
+router.get('/pastebin/:paste', pastebinHandle)
 
 export default {
   async fetch(request, env, context) {
-    return router.handle(request, env, context);
-  },
-};
+    return router.handle(request, env, context)
+  }
+}
--- worker/src/twitch.js
+++ worker/src/twitch.js
@@ -1,71 +1,71 @@
-import { handleFetchData, responseWith, status } from "./utils.js";
+import { handleFetchData, responseWith, status } from './utils.js'
 
 async function getAccessToken(code, env, host) {
-  const clientId = env.TWITCH_CLIENT_ID;
-  const clientSecret = env.TWITCH_CLIENT_SECRET;
-  const redirectUri = `https://${host}/auth/callback`;
+  const clientId = env.TWITCH_CLIENT_ID
+  const clientSecret = env.TWITCH_CLIENT_SECRET
+  const redirectUri = `https://${host}/auth/callback`
 
-  const formData = new FormData();
-  formData.append("client_id", clientId);
-  formData.append("client_secret", clientSecret);
-  formData.append("code", code);
-  formData.append("grant_type", "authorization_code");
-  formData.append("redirect_uri", redirectUri);
+  const formData = new FormData()
+  formData.append('client_id', clientId)
+  formData.append('client_secret', clientSecret)
+  formData.append('code', code)
+  formData.append('grant_type', 'authorization_code')
+  formData.append('redirect_uri', redirectUri)
 
   try {
-    const accessData = await fetch("https://id.twitch.tv/oauth2/token", {
-      method: "POST",
-      body: formData,
-    });
+    const accessData = await fetch('https://id.twitch.tv/oauth2/token', {
+      method: 'POST',
+      body: formData
+    })
 
     if (accessData?.status != status.OK) {
-      const { message } = await handleFetchData(response);
-      throw new Error(message);
+      const { message } = await handleFetchData(response)
+      throw new Error(message)
     }
 
-    const accessToken = await handleFetchData(accessData);
-    return accessToken;
+    const accessToken = await handleFetchData(accessData)
+    return accessToken
   } catch (error) {
-    console.log(error);
-    throw new Error(error.message);
+    console.log(error)
+    throw new Error(error.message)
   }
 }
 
 export async function twitchAuthHandle({ env, req }) {
-  const url = new URL(req.url);
-  const { code } = req.query;
+  const url = new URL(req.url)
+  const { code } = req.query
 
   try {
-    if (code === null || code === "" || typeof code === "undefined") {
+    if (code === null || code === '' || typeof code === 'undefined') {
       return responseWith(
-        "The code query oarameter is required",
-        status.BadRequest,
-      );
+        'The code query oarameter is required',
+        status.BadRequest
+      )
     }
 
-    const accessToken = await getAccessToken(code, env, url.host);
-    await env.KV.put(code, JSON.stringify(accessToken));
+    const accessToken = await getAccessToken(code, env, url.host)
+    await env.KV.put(code, JSON.stringify(accessToken))
 
-    return Response.redirect(`https://${url.host}/clip/create/${code}`, 301);
+    return Response.redirect(`https://${url.host}/clip/command/${code}`, 301)
   } catch (error) {
-    console.log(error);
-    return responseWith(error.message, status.BadRequest);
+    console.log(error)
+    return responseWith(error.message, status.BadRequest)
   }
 }
 
 async function getChannelId(channel) {
   try {
-    const data = await fetch(`https://decapi.me/twitch/id/${channel}`);
-    const channelId = await handleFetchData(data);
+    const data = await fetch(`https://decapi.me/twitch/id/${channel}`)
+    const channelId = await handleFetchData(data)
 
-    if (channelId.includes("User not found")) {
-      throw new Error(channelId);
+    if (channelId.includes('User not found')) {
+      throw new Error(channelId)
     }
 
-    return channelId;
+    return channelId
   } catch (error) {
-    console.log(error);
-    throw new Error(error.message);
+    console.log(error)
+    throw new Error(error.message)
   }
 }
 
@@ -74,60 +74,60 @@ async function createClip(accessToken, channelId, clientId) {
     const response = await fetch(
       `https://api.twitch.tv/helix/clips?broadcaster_id=${channelId}`,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          "Client-Id": clientId,
-        },
-      },
-    );
+          'Client-Id': clientId
+        }
+      }
+    )
 
     if (response?.status != status.Accepted) {
-      const { message } = await handleFetchData(response);
-      throw new Error(message);
+      const { message } = await handleFetchData(response)
+      throw new Error(message)
     }
 
     const {
-      data: [{ id }],
-    } = await handleFetchData(response);
+      data: [{ id }]
+    } = await handleFetchData(response)
 
-    return `https://clips.twitch.tv/${id}`;
+    return `https://clips.twitch.tv/${id}`
   } catch (error) {
-    console.log(error);
-    throw new Error(error.message);
+    console.log(error)
+    throw new Error(error.message)
   }
 }
 
 export async function createClipHandle({ env, req }) {
-  const token = req.params.id;
-  const { channel } = req.query;
+  const token = req.params.id
+  const { channel } = req.query
 
   try {
-    const channelId = await env.KV.get(channel);
-    const accessToken = JSON.parse(await env.KV.get(token));
+    const channelId = await env.KV.get(channel)
+    const accessToken = JSON.parse(await env.KV.get(token))
 
     if (channelId === null) {
-      const _channelId = await getChannelId(channel);
-      await env.KV.put(channel, _channelId);
+      const _channelId = await getChannelId(channel)
+      await env.KV.put(channel, _channelId)
 
       const clipUrl = await createClip(
         accessToken.access_token,
         _channelId,
-        env.TWITCH_CLIENT_ID,
-      );
+        env.TWITCH_CLIENT_ID
+      )
 
-      return responseWith(clipUrl, status.OK);
+      return responseWith(clipUrl, status.OK)
     } else {
       const clipUrl = await createClip(
         accessToken.access_token,
         channelId,
-        env.TWITCH_CLIENT_ID,
-      );
+        env.TWITCH_CLIENT_ID
+      )
 
-      return responseWith(clipUrl, status.OK);
+      return responseWith(clipUrl, status.OK)
     }
   } catch (error) {
-    console.log(error);
-    return responseWith(error.message, status.OK);
+    console.log(error)
+    return responseWith(error.message, status.OK)
   }
 }
